3d79de8db8471b4d88e2565e35e7ad8b9b3a201a
lloyd85 <i_am_lloyd@gmail.com>
2014-08-12 00:39:01 +0100

    Built site from commit 7f96718 on branch master

---
+++ dist/web.js
@@ -0,0 +1,13 @@
+/**
+ * Created by lloydntim on 11/08/2014.
+ */
+
+'use strict'
+
+var gzippo = require('gzippo');
+var express = require('express');
+var app = express();
+
+app.use(express.logger('dev'));
+app.use(gzippo.staticGzip("" + __dirname + "/dist"));
+app.listen(process.env.PORT || 5000);
